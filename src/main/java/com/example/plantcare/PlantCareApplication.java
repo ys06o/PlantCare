82b44515511a501fd1fb86074bd21ecbe637852d
ys06o <xoy5753@sungkyul.ac.kr>
2026-03-03 22:59:37 +0900

    Initial commit

---
+++ src/main/java/com/example/plantcare/PlantCareApplication.java
@@ -0,0 +1,12 @@
+package com.example.plantcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantCareApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlantCareApplication.class, args);
+    }
+
+}
