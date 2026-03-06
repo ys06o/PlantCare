d5dda65a02c4aea2c30f8ee1e3ed4f008a1f53c2
junhyunsim0402 <simjjang11@gmail.com>
2026-03-06 18:22:34 +0900

    feat : 1. 특정 사용자의 AI진단 목록 조회 기능

--- src/test/java/com/example/plantcare/repository/AIDiagnosisRepository.java
+++ src/test/java/com/example/plantcare/repository/AIDiagnosisRepository.java
@@ -4,7 +4,13 @@ import com.example.plantcare.entity.AIDiagnosisEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AIDiagnosisRepository
-        extends JpaRepository<AIDiagnosisEntity,Integer> {
+        extends JpaRepository<AIDiagnosisEntity,Long> {
+    List<AIDiagnosisEntity> findByPlant_User_UserId(int userId);
 }
+// findByPlant로 findBy라는 조건문으로
+// Entity파일의 Plant라는 변수명을 찾고
+// 유저테이블의 User_id라는 조건으로 찾는다 ->없으면 빈 리스트로 반환
--- src/test/java/com/example/plantcare/service/AIDiagnosisService.java
+++ src/test/java/com/example/plantcare/service/AIDiagnosisService.java
@@ -1,10 +1,39 @@
 package com.example.plantcare.service;
 
+import com.example.plantcare.dto.AIDiagnosisDto;
+import com.example.plantcare.entity.AIDiagnosisEntity;
 import com.example.plantcare.repository.AIDiagnosisRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class AIDiagnosisService {
     @Autowired private AIDiagnosisRepository aiDiagnosisRepository;
+
+    // 전체적으로 AI진단한 결과 및 내용 등 전체 출력 ( 관리자용 ) 모든 사용자의 진단결과 출력
+    // 안쓸수도 있음
+    public List<AIDiagnosisEntity> findAll(){
+        List<AIDiagnosisEntity> AIList=aiDiagnosisRepository.findAll();
+        List<AIDiagnosisDto> AIDtoList=new ArrayList<>();
+        AIList.forEach(entity->{
+            AIDtoList.add(entity.toDto());
+        });
+        return AIList;
+    }
+
+    // 1. 특정 사용자의 진단목록( 전체 )
+    public List<AIDiagnosisDto> UserFindAll(int userId){
+        // 각 유저의 자신의 진단기록을 리스트에 넣은 코드
+        List<AIDiagnosisEntity> AIEntityList=aiDiagnosisRepository.findByPlant_User_UserId(userId);
+        List<AIDiagnosisDto> AIDtoList=new ArrayList<>();
+        AIEntityList.forEach(entity->{
+            AIDtoList.add(entity.toDto());
+        });
+        return AIDtoList;
+    }
+
+    // 1번을 이용한 진단목록 상세 조회
 }
